5fe86671016e7cd3fb34851699f00965efceb30f
Vimarsh <vimarsh244@gmail.com>
2023-08-27 00:53:17 +0530

    all commit timings are messed up, cause NTP didnt work

--- robotics_hackathon_automation/src/controller.py
+++ robotics_hackathon_automation/src/controller.py
@@ -1,66 +1,49 @@
-#!/usr/bin/env python3
-# Remember to change the coordinates recieved by the planner from (X, Y) to (X + 1.79, Y + 0.66).
-#you need to name this node "controller"
-
-#!/usr/bin/env python
-
 import rospy
-from geometry_msgs.msg import Twist
 from nav_msgs.msg import Odometry
-from math import atan2, sqrt
+from tf.transformations import euler_from_quaternion
+from geometry_msgs.msg import Point, Twist
+from math import atan2
 
-class TurtleBotController:
-    def __init__(self):
-        rospy.init_node('turtlebot_controller', anonymous=True)
-        self.velocity_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
-        self.odom_subscriber = rospy.Subscriber('/odom', Odometry, self.odom_callback)
-        self.rate = rospy.Rate(10)  # 10 Hz
-        self.current_pose = Odometry().pose.pose
-        self.original_target_points = [(-5.06, -3.12), (-5.030810611572746, -2.964564276874691), (-5.006301016800769, -2.772969051635586)]
-        self.target_points = [(x + 1.79, y + 0.66) for x, y in self.original_target_points]
-        self.current_target = 0
+x = 0.0
+y = 0.0 
+theta = 0.0
 
-    def odom_callback(self, odom_msg):
-        print(odom_msg.pose.pose)
-        self.current_pose = odom_msg.pose.pose
+def newOdom(msg):
+    global x
+    global y
+    global theta
 
-    def get_distance_to_target(self, target_point):
-        if self.current_pose is None:
-            return float('inf')
-        dx = self.current_pose.position.x - target_point[0]
-        dy = self.current_pose.position.y - target_point[1]
-        return sqrt(dx**2 + dy**2)
+    x = msg.pose.pose.position.x
+    y = msg.pose.pose.position.y
 
-    def navigate_to_next_point(self):
-        target_point = self.target_points[self.current_target]
-        while self.get_distance_to_target(target_point) > 0.1:
-            dx = target_point[0] - self.current_pose.position.x
-            # self.current_pose.position.x
-            dy = target_point[1] - self.current_pose.position.y
-            desired_angle = atan2(dy, dx)
-            
-            cmd_vel = Twist()
-            cmd_vel.linear.x = 0.1  # adjust the linear velocity
-            cmd_vel.angular.z = 0.5 * (desired_angle - self.get_yaw())
-            
-            self.velocity_publisher.publish(cmd_vel)
-            self.rate.sleep()
+    rot_q = msg.pose.pose.orientation
+    (roll, pitch, theta) = euler_from_quaternion([rot_q.x, rot_q.y, rot_q.z, rot_q.w])
 
-        self.current_target += 1
-        if self.current_target >= len(self.target_points):
-            rospy.loginfo("All points reached!")
-            rospy.signal_shutdown("All points reached")
+rospy.init_node("speed_controller")
 
-    def get_yaw(self):
-        return self.current_pose.orientation.z  # You might need to adjust this to get the yaw angle properly
+sub = rospy.Subscriber("/odometry/filtered", Odometry, newOdom)
+pub = rospy.Publisher("/cmd_vel", Twist, queue_size = 1)
 
-    def run(self):
-        while not rospy.is_shutdown() and self.current_target < len(self.target_points):
-            self.navigate_to_next_point()
+speed = Twist()
 
-if __name__ == '__main__':
-    try:
-        controller = TurtleBotController()
-        controller.run()
-    except rospy.ROSInterruptException:
-        pass
+r = rospy.Rate(4)
+
+goal = Point()
+goal.x = 5
+goal.y = 5
+
+while not rospy.is_shutdown():
+    inc_x = goal.x -x
+    inc_y = goal.y -y
+
+    angle_to_goal = atan2(inc_y, inc_x)
+
+    if abs(angle_to_goal - theta) > 0.1:
+        speed.linear.x = 0.0
+        speed.angular.z = 0.3
+    else:
+        speed.linear.x = 0.5
+        speed.angular.z = 0.0
+
+    pub.publish(speed)
+    r.sleep()    
